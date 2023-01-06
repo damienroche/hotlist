2bc9013c4d16682db4cc1feefcf4af710be93dd6
Damien Roche <damienroche@free.fr>
2023-01-06 13:15:14 +0100

    create new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,24 @@
 class ListsController < ApplicationController
+  before_action :set_list, :only => [:show]
+
   def new
+    @list = List.new
+  end
+  
+  def show
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.create(list_params)
+    redirect_to list_path(@list)
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title)
   end
 end
